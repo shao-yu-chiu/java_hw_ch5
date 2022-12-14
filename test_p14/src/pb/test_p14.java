0a49a767c4a134eead22c5f78a213219138fd15d
shao-yu-chiu <lulala1206@gmail.com>
2022-12-14 19:31:16 +0800

    first

---
+++ test_p14/src/pb/test_p14.java
@@ -0,0 +1,12 @@
+package pb;
+//109360142ªô¤ÖÅA
+import pc.Car;
+public class test_p14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		car1.show();
+	}
+
+}
---
+++ test_p8/src/pa/test_p8.java
@@ -0,0 +1,11 @@
+package pa;
+//109360142ªô¤ÖÅA
+public class test_p8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		car1.show();
+	}
+
+}
